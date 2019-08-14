fd7999c6680386c70408ae06b3c29764e643f470
Muhsin Zyne <muhsin.zyne@gmail.com>
2019-08-14 23:32:51 +0530

    test cases commit

--- tests/unit/SegmentLedTest.php
+++ tests/unit/SegmentLedTest.php
@@ -9,24 +9,28 @@ class SegmentLedTest extends PHPUnit_Framework_TestCase {
 		$this->segment = new SegmentLed();
 	}
 
-	public function test_segment_number_list_exists() {
+	public function test_segment_number_list_exists() 
+	{
 		$number_list_keys = [0,1,2,3,4,5,6,7,8,9];
 		foreach ($number_list_keys as $value) {
 			$this->assertArrayHasKey($value, $this->segment->number_list);
 		}
 	}
-	public function test_led_power_usage_amount() {
+	public function test_led_power_usage_amount() 
+	{
 		// assuming the value as 16
 		$this->assertEquals($this->segment->calculate_led_power('01:01'), 16);
 	}
 
-	public function test_script_response() {
+	public function test_script_response() 
+	{
 		$response = (array) $this->segment->calculatePowerUsage('max');
 		$this->assertArrayHasKey('power_usage', $response);
 		$this->assertArrayHasKey('time', $response);
 	}
 
-	public function test_script_respose_for_min_value() {
+	public function test_script_respose_for_min_value() 
+	{
 		$response = $this->segment->calculatePowerUsage('min');
 		$this->assertEquals($response->power_usage, 8);
 		$this->assertEquals($response->time, '11:11');
